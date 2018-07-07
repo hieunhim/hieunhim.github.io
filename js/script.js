3d836a31da0f0794e572ffc8a6af3d00615c3d38
hieudv <hieudv196@gmail.com>
2018-07-07 11:12:41 +0700

    menu-left

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenavTop").style.width = "250px";
   }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenavTop").style.width = "0";
 }
 
 $(document).ready(function(){
@@ -33,7 +18,38 @@ $(document).ready(function(){
         }
     );
 });
+jQuery(document).ready(function($) {
+    "use strict";
+// Fiter......................................................................................
+    $('.button2').on('click', function(){
+    $("#mySidenavv").css("width","300px");
+    $(".collapsee").css("visibility","visible");
+});
 
-$(".logo-buy , .logo.search").on("click", function() {
-  $(this).parent().find('.shopping-cart-content , .dropdown-menu.timkiem , .top').slideToggle('medium');
-})
+$('.closebtn').on('click', function(){
+    $("#mySidenavv").css("width","0");
+    $(".collapsee").css("visibility","hidden");
+});
+});
+$('#btn1').on('click', function(){
+    var x= $("#btn1").attr("aria-expanded");
+    if(x=="true"){
+        $(".up1").css("display","none");
+        $(".down1").css("display","inline-block");
+    }
+    else{
+         $(".down1").css("display","none");
+         $(".up1").css("display","inline-block");
+    }
+});
+$('#btn4').on('click', function(){
+    var x= $("#btn4").attr("aria-expanded");
+    if(x=="true"){
+        $(".up4").css("display","none");
+        $(".down4").css("display","inline-block");
+    }
+    else{
+         $(".down4").css("display","none");
+         $(".up4").css("display","inline-block");
+    }
+});
